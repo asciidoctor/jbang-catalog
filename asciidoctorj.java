aafc55b0e2a7d60d13d9adb848ffda631a2d34a0
Werner Fouché <werner.fouche@gmail.com>
2025-10-23 12:03:51 +0200

    Updated links to Maven Central catalog

--- asciidoctorj.java
+++ asciidoctorj.java
@@ -2,44 +2,44 @@
 
 // The same list of JARs that appear in the asciidoctorj shell script
 
-// https://mvnrepository.com/artifact/org.asciidoctor/asciidoctorj
 //DEPS org.asciidoctor:asciidoctorj:3.0.0
+// - https://mvnrepository.com/artifact/org.asciidoctor/asciidoctorj
 
-// https://mvnrepository.com/artifact/org.asciidoctor/asciidoctorj-api
 //DEPS org.asciidoctor:asciidoctorj-api:3.0.0
+// - https://mvnrepository.com/artifact/org.asciidoctor/asciidoctorj-api
 
-// https://mvnrepository.com/artifact/org.asciidoctor/asciidoctorj-cli
 //DEPS org.asciidoctor:asciidoctorj-cli:3.0.0
+// - https://mvnrepository.com/artifact/org.asciidoctor/asciidoctorj-cli
 
-// https://mvnrepository.com/artifact/org.asciidoctor/asciidoctorj-epub3
 //DEPS org.asciidoctor:asciidoctorj-epub3:2.2.0
+// - https://mvnrepository.com/artifact/org.asciidoctor/asciidoctorj-epub3
 
-// https://mvnrepository.com/artifact/org.asciidoctor/asciidoctorj-diagram
 //DEPS org.asciidoctor:asciidoctorj-diagram:3.0.1
+// - https://mvnrepository.com/artifact/org.asciidoctor/asciidoctorj-diagram
 
-// https://mvnrepository.com/artifact/org.asciidoctor/asciidoctorj-diagram-batik
 //DEPS org.asciidoctor:asciidoctorj-diagram-batik:1.17
+// - https://mvnrepository.com/artifact/org.asciidoctor/asciidoctorj-diagram-batik
 
-// https://mvnrepository.com/artifact/org.asciidoctor/asciidoctorj-diagram-ditaamini
 //DEPS org.asciidoctor:asciidoctorj-diagram-ditaamini:1.0.3
+// - https://mvnrepository.com/artifact/org.asciidoctor/asciidoctorj-diagram-ditaamini
 
-// https://mvnrepository.com/artifact/org.asciidoctor/asciidoctorj-diagram-plantuml
 //DEPS org.asciidoctor:asciidoctorj-diagram-plantuml:1.2025.3
+// - https://mvnrepository.com/artifact/org.asciidoctor/asciidoctorj-diagram-plantuml
 
-// https://mvnrepository.com/artifact/org.asciidoctor/asciidoctorj-diagram-jsyntrax
 //DEPS org.asciidoctor:asciidoctorj-diagram-jsyntrax:1.38.2
+// - https://mvnrepository.com/artifact/org.asciidoctor/asciidoctorj-diagram-jsyntrax
 
-// https://mvnrepository.com/artifact/org.asciidoctor/asciidoctorj-pdf
 //DEPS org.asciidoctor:asciidoctorj-pdf:2.3.21
+// - https://mvnrepository.com/artifact/org.asciidoctor/asciidoctorj-pdf
 
-// https://mvnrepository.com/artifact/org.asciidoctor/asciidoctorj-revealjs
 //DEPS org.asciidoctor:asciidoctorj-revealjs:5.2.0
+// - https://mvnrepository.com/artifact/org.asciidoctor/asciidoctorj-revealjs
 
-// https://mvnrepository.com/artifact/com.beust/jcommander
 // --DEPS com.beust:jcommander:1.82
+// - https://mvnrepository.com/artifact/com.beust/jcommander
 
-// https://mvnrepository.com/artifact/org.jruby/jruby-complete
 // --DEPS org.jruby:jruby-complete:9.4.8.0
+// - https://mvnrepository.com/artifact/org.jruby/jruby-complete
 
 import java.io.IOException;
 
